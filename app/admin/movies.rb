e0cf676ba2219d9206cd640530b02ffae3b9b50e
damari <motokin128@gmail.com>
2021-07-08 20:02:08 +0900

    admin/movie.rb admin/text.rb に追記
    管理者画面からテキスト教材、動画教材を投稿できるように実装するため

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Movie do
 
+  permit_params :genre, :title, :url
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/admin/texts.rb
+++ app/admin/texts.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Text do
 
+  permit_params :genre, :title, :content
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
