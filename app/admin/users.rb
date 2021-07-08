d2c3aa467fa6e7ea6fc5120b400d8201874abcbb
damari <motokin128@gmail.com>
2021-07-08 20:25:25 +0900

    admin/user.rb に追記
    管理者画面からユーザーを作成できるように実装するため

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register User do
 
+  permit_params :email, :password, :password_confirmation
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
