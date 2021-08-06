615a0b80c7a42c94c354384c81fab9466c7d4398
Orizin <nlgporizin10@gmail.com>
2021-08-07 00:38:59 +0900

    動画教材ページのジャンル分け

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem "rouge"
 gem "sass-rails", ">= 6"
 gem "turbolinks", "~> 5"
 gem "webpacker", "~> 5.0"
+gem 'kaminari'
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
+  PER_PAGE = 20
+
   def index
-    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST)
+    @movies = if params[:genre]
+                Movie.where(genre: "php").page(params[:page]).per(PER_PAGE)
+              else
+                Movie.where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+              end
   end
 end
